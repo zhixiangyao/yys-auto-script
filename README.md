7cd4e46adf9d69330e95525dbbb1f4ec3509eee9
zhixiangyao <blueskyzx@icloud.com>
2022-06-10 22:28:57 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# yys-auto-script
- 阴阳师自动脚本
+# 阴阳师自动脚本
+
+阴阳师自动脚本(只能在 macOS 上使用)
+
+## 背景
+
+- macOS
+  - Homebrew
+    - cliclick
+  - Node.js
+    - zx
+  - 阴阳师
+
+# 安装
+
+前置条件 homebrew
+
+```bash
+homebrew install cliclick # 获取鼠标坐标
+homebrew install node # JavaScript 运行时环境
+# 安装好 node 后，安装 zx
+npm install --global zx
+```
+
+## run
+
+```bash
+# 先把鼠标放到 “开始” 获取鼠标坐标
+cliclick p
+```
+
+```bash
+# -c 是 count 次数
+# -s 是 seconds 是延迟时间
+# -p 是 position 坐标
+zx ~/xxx/yys-auto.mjs -c=1000 -s=21 -p=793,570
+```
---
+++ scripts/yys-auto.mjs
@@ -0,0 +1,88 @@
+#!/usr/bin/env zx
+
+/**
+ *
+ * @param {number} number
+ * @returns {number}
+ */
+function generatorRandomNumber(number) {
+  return Math.floor(Math.random() * number)
+}
+
+/**
+ *
+ * @param {`-${string}=${string}`[]} args
+ * @returns
+ */
+async function getParams(args) {
+  let c = 1 //  count   数量
+  let s = 1 //  seconds 秒
+
+  let px = 0 // position x
+  let py = 0 // position y
+
+  const wX = 6 // wave x 轴波动范围
+  const wY = 6 // wave y 轴波动范围
+
+  if (args.length !== 0) {
+    for (const arg of args) {
+      const [tag, value] = arg.split('=')
+
+      switch (tag) {
+        case '-c':
+          c = parseInt(value)
+          break
+
+        case '-s':
+          s = parseInt(value)
+          break
+
+        case '-p':
+          if (','.includes(',')) {
+            const [x, y] = value.trim().split(',')
+            px = parseInt(x)
+            py = parseInt(y)
+          }
+          break
+
+        default:
+          console.log(`Unknown argument: ${tag}`)
+          break
+      }
+    }
+  }
+
+  return { c, s, px, py, wX, wY }
+}
+
+async function main() {
+  const [, , , ...args] = process.argv
+  const { c, s, px, py, wX, wY } = await getParams(args)
+
+  for (let i = 0; i < c; i++) {
+    const halfWX = Math.floor(wX / 2)
+    const halfWY = Math.floor(wY / 2)
+
+    let x = px - halfWX + generatorRandomNumber(wX)
+    let y = py - halfWY + generatorRandomNumber(wY)
+
+    x = x < 0 ? 0 : x
+    y = y < 0 ? 0 : y
+
+    const position = `${x},${y}`
+
+    await $`cliclick c:${position}`
+    await $`cliclick c:${position}`
+    await $`sleep ${s}`
+
+    await $`cliclick c:${position}`
+    await $`cliclick c:${position}`
+    await $`sleep 3`
+
+    console.log(`第 ${i} 次`)
+  }
+
+  console.log(`echo 执行了 ${c} 次, 每次等待 ${s} 秒`)
+}
+
+main()
